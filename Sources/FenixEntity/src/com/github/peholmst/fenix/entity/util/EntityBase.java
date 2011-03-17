2dec48e19571f3e75889d8e9d4f48988456f16ad
Petter Holmström <petter.holmstrom@abo.fi>
2011-03-17 13:34:30 +0200

    Added support for creating transient copies of an entity graph.

--- Sources/FenixEntity/src/com/github/peholmst/fenix/entity/util/EntityBase.java
+++ Sources/FenixEntity/src/com/github/peholmst/fenix/entity/util/EntityBase.java
@@ -15,30 +15,40 @@
  */
 package com.github.peholmst.fenix.entity.util;
 
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Map;
+
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
 
+import com.github.peholmst.fenix.common.util.CloneThis;
 import com.github.peholmst.fenix.common.util.CloneUtil;
+import com.github.peholmst.fenix.common.util.FieldUtil;
 
 /**
  * This is a base class for all entities. It includes an identifier and a
  * version number for optimistic locking. Equality and hash code is based on the
  * identifier alone and therefore it is assigned already when the entity
- * instance is created. Please see the factory methods (
- * {@link #createEntity(Class)} and
- * {@link #createEntityWithExistingId(Class, long)}) for more information.
+ * instance is created. Please see the factory methods for more information.
  * <p>
  * Cloning is handled by {@link CloneUtil#deepClone(Cloneable)}, which means
  * that subclasses can use the {@link CloneThis} annotation if they want to.
+ * Please note, that the <code>clone()</code> method will make an exact clone of
+ * the entity graph, including the identifiers. If you want a copy of the entity
+ * graph but with different identifiers, you should use
+ * {@link #createFromExistingEntity(EntityBase)}.
+ * 
+ * @see #createEntity(Class)
+ * @see #createEntityWithExistingId(Class, long)
+ * @see #createFromExistingEntity(EntityBase)
  * 
  * @author Petter Holmström
  */
 @MappedSuperclass
 public abstract class EntityBase implements java.io.Serializable, Cloneable {
 
-	// TODO Test me!
-	
 	private static final long serialVersionUID = 9158596445082221718L;
 
 	@Id
@@ -74,6 +84,111 @@ public abstract class EntityBase implements java.io.Serializable, Cloneable {
 	public static <T extends EntityBase> T createEntity(Class<T> entityClass) {
 		return createEntityWithExistingId(entityClass,
 				IdGenerator.getNextValue());
+	}
+
+	/**
+	 * Creates a new transient entity instance that is a copy of the specified
+	 * entity. This means that all entity identifiers have been regenerated and
+	 * the optimistic locking versions set to <code>null</code>. All fields
+	 * annotated with {@link CloneThis} have also been made transient
+	 * recursively. Maps, arrays and Collections are made transient if
+	 * {@link CloneThis#deepClone()} is true.
+	 * 
+	 * @param <T>
+	 *            the type of the entity to create.
+	 * @param original
+	 *            the original entity.
+	 * @return a new entity instance.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T extends EntityBase> T createFromExistingEntity(T original) {
+		assert original != null : "oldEntity must not be null";
+		T newEntity = (T) original.clone();
+		try {
+			newEntity.makeTransient();
+		} catch (Exception e) {
+			throw new IllegalArgumentException(
+					"Could not create a new entity based on the existing one",
+					e);
+		}
+		return newEntity;
+	}
+
+	private void makeTransient() throws Exception {
+		makeObjectTransient(this);
+	}
+
+	private static void makeObjectTransient(final Object object)
+			throws Exception {
+		if (object == null) {
+			return;
+		}
+		if (object instanceof EntityBase) {
+			((EntityBase) object).identifier = IdGenerator.getNextValue();
+			((EntityBase) object).optLockVersion = null;
+		}
+		FieldUtil.visitAllDeclaredFields(object.getClass(),
+				new FieldUtil.AccessibleFieldVisitor() {
+
+					@Override
+					public void visitAccessibleField(Field field)
+							throws Exception {
+						CloneThis cloneAnnotation = field
+								.getAnnotation(CloneThis.class);
+						if (cloneAnnotation != null) {
+							if (field.getType().isArray()) {
+								makeArrayFieldTransient(field, object,
+										cloneAnnotation);
+							} else {
+								makeObjectFieldTransient(field, object,
+										cloneAnnotation);
+							}
+						}
+					}
+				});
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void makeObjectFieldTransient(Field field, Object owner,
+			CloneThis cloneAnnotation) throws Exception {
+		Object value = field.get(owner);
+		if (value instanceof Collection) {
+			makeCollectionTransient((Collection<Object>) value, cloneAnnotation);
+		} else if (value instanceof Map) {
+			makeMapTransient((Map<Object, Object>) value, cloneAnnotation);
+		} else {
+			makeObjectTransient(value);
+		}
+	}
+
+	private static void makeCollectionTransient(Collection<Object> collection,
+			CloneThis cloneAnnotation) throws Exception {
+		if (cloneAnnotation.deepClone()) {
+			for (Object collectionItem : collection) {
+				makeObjectTransient(collectionItem);
+			}
+		}
+	}
+
+	private static void makeMapTransient(Map<Object, Object> map,
+			CloneThis cloneAnnotation) throws Exception {
+		if (cloneAnnotation.deepClone()) {
+			for (Object value : map.values()) {
+				makeObjectTransient(value);
+			}
+		}
+	}
+
+	private static void makeArrayFieldTransient(Field field, Object owner,
+			CloneThis cloneAnnotation) throws Exception {
+		if (cloneAnnotation.deepClone()) {
+			Object[] value = (Object[]) field.get(owner);
+			if (value != null) {
+				for (Object arrayItem : value) {
+					makeObjectTransient(arrayItem);
+				}
+			}
+		}
 	}
 
 	/**
@@ -126,7 +241,7 @@ public abstract class EntityBase implements java.io.Serializable, Cloneable {
 	 * @return true if it is persistent, false if it is transient.
 	 */
 	public boolean isPersistent() {
-		return optLockVersion != null;
+		return getOptLockVersion() != null;
 	}
 
 	@Override
