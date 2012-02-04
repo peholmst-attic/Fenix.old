35859b57550a807c651bcd21fc0f90c565a23f29
Petter Holmstrom <petter.holmstrom@abo.fi>
2012-02-04 23:00:54 +0200

    Added DDD annotations

---
+++ src/FenixBase/src/fenix/base/domain/annotation/ValueObject.java
@@ -0,0 +1,33 @@
+/*
+ * Fenix
+ * Copyright (C) 2012 Petter Holmström
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fenix.base.domain.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Annotation used to mark objects that act as value objects. This annotation is
+ * only used to make it easier for developers to see which objects are
+ * considered value objects. It has no technical functionality.
+ * 
+ * @author Petter Holmström
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface ValueObject {
+
+}
