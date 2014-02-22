0e55daba56f76c8dc4975c716e1df76a3cbf0312
Petter Holmström <petter.holmstrom@abo.fi>
2014-02-22 22:25:17 +0200

    Added UserServiceBean

---
+++ java/shared/src/main/java/net/pkhapps/fenix/security/UserServiceBean.java
@@ -0,0 +1,52 @@
+/*
+ * Fenix
+ * Copyright (C) 2014 Petter Holmström
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.pkhapps.fenix.security;
+
+import net.pkhapps.fenix.entity.User;
+import net.pkhapps.fenix.entity.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Implementation of {@link UserDetailsService} that invokes a
+ * {@link UserRepository} to look up users.
+ *
+ * @author Petter Holmström
+ */
+@Service
+public class UserServiceBean implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No such user");
+        }
+        return user;
+    }
+
+}
