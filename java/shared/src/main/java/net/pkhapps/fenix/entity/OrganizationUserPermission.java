642a5dddff1027a31e8cc9fde3feed57cb43c746
Petter Holmström <petter.holmstrom@abo.fi>
2014-02-22 22:24:53 +0200

    Added UserRepository and Organization entities

---
+++ java/shared/src/main/java/net/pkhapps/fenix/entity/OrganizationUserPermission.java
@@ -0,0 +1,49 @@
+/*
+ * Fenix
+ * Copyright (C) 2014 Petter Holmström
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.pkhapps.fenix.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ * Embeddable that represents a permission that is granted to a
+ * {@link OrganizationUser}.
+ *
+ * @author Petter Holmström
+ */
+@Embeddable
+public class OrganizationUserPermission implements GrantedAuthority {
+
+    @Column(name = "permission", nullable = false)
+    private String permission = "";
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    @Override
+    public String getAuthority() {
+        return "UPERM_" + permission;
+    }
+
+}
