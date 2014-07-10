5c15afa2c53b02dfd9087338dfc384ea88f57982
Petter Holmström <petter.holmstrom@abo.fi>
2014-07-10 22:48:57 +0300

    I18N classes were migrated to Vaadin4Spring

---
+++ java/fenix-core/src/main/java/net/pkhapps/fenix/core/navigation/NavigationRequest.java
@@ -0,0 +1,11 @@
+package net.pkhapps.fenix.core.navigation;
+
+/**
+ * Created by peholmst on 2014-06-19.
+ */
+public class NavigationRequest {
+
+    public String getNavigationState() {
+        return "";
+    }
+}
