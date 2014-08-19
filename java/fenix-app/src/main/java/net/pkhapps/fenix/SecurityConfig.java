93e46af3dcd73d403ea6eb16c6847df0e4d0cd9d
Petter Holmström <petter.holmstrom@abo.fi>
2014-08-19 22:36:24 +0300

    Added password encoder

--- java/fenix-app/src/main/java/net/pkhapps/fenix/SecurityConfig.java
+++ java/fenix-app/src/main/java/net/pkhapps/fenix/SecurityConfig.java
@@ -2,6 +2,7 @@ package net.pkhapps.fenix;
 
 import net.pkhapps.fenix.core.security.FenixUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,9 +11,10 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
- * Created by peholmst on 2014-08-02.
+ * Security related system configuration
  */
 @Configuration
 @EnableWebSecurity
@@ -24,9 +26,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     FenixUserDetailsService fenixUserDetailsService;
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(fenixUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        auth.userDetailsService(fenixUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
