03f598eff91a454c741f2bdaa81d36ad9d371cb6
Petter Holmström <petter@vaadin.com>
2014-09-10 16:03:37 +0300

    Refactored repository interfaces

--- java/fenix-communication/src/main/java/net/pkhapps/fenix/communication/entity/ContactGroupRepository.java
+++ java/fenix-communication/src/main/java/net/pkhapps/fenix/communication/entity/ContactGroupRepository.java
@@ -1,14 +1,9 @@
 package net.pkhapps.fenix.communication.entity;
 
-import net.pkhapps.fenix.core.entity.FireDepartment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import net.pkhapps.fenix.core.entity.FireDepartmentSpecificEntityRepository;
 
 /**
  * Repository of {@link net.pkhapps.fenix.communication.entity.ContactGroup}s.
  */
-public interface ContactGroupRepository extends JpaRepository<ContactGroup, Long> {
-
-    List<ContactGroup> findByFireDepartment(FireDepartment fireDepartment);
+public interface ContactGroupRepository extends FireDepartmentSpecificEntityRepository<ContactGroup> {
 }
--- java/fenix-communication/src/main/java/net/pkhapps/fenix/communication/entity/ContactRepository.java
+++ java/fenix-communication/src/main/java/net/pkhapps/fenix/communication/entity/ContactRepository.java
@@ -1,14 +1,9 @@
 package net.pkhapps.fenix.communication.entity;
 
-import net.pkhapps.fenix.core.entity.FireDepartment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import net.pkhapps.fenix.core.entity.FireDepartmentSpecificEntityRepository;
 
 /**
  * Repository of {@link net.pkhapps.fenix.communication.entity.Contact}s.
  */
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-
-    List<Contact> findByFireDepartment(FireDepartment fireDepartment);
+public interface ContactRepository extends FireDepartmentSpecificEntityRepository<Contact> {
 }
