5495bfcdb0b547c6ededc87fb6d91bd2d144daaf
Petter Holmström <petter@vaadin.com>
2014-09-10 08:45:02 +0300

    Minor changes to Contact and ContactRepository

--- java/fenix-communication/src/main/java/net/pkhapps/fenix/communication/entity/Contact.java
+++ java/fenix-communication/src/main/java/net/pkhapps/fenix/communication/entity/Contact.java
@@ -3,10 +3,18 @@ package net.pkhapps.fenix.communication.entity;
 import com.google.gwt.thirdparty.guava.common.base.Strings;
 import net.pkhapps.fenix.core.entity.AbstractFireDepartmentSpecificEntity;
 
-import javax.persistence.*;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -44,57 +52,57 @@ public class Contact extends AbstractFireDepartmentSpecificEntity {
     @Column(name = "method", nullable = false)
     @CollectionTable(name = "contact_communication_methods",
             joinColumns = @JoinColumn(name = "contact_id", nullable = false))
-    private Set<CommunicationMethod> communicationMethods = Collections.emptySet();
+    private Set<CommunicationMethod> communicationMethods = new HashSet<>();
 
-    protected Contact() {
+    public Contact() {
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    protected void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFirstName(String firstName) {
+        this.firstName = Strings.nullToEmpty(firstName);
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    protected void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setLastName(String lastName) {
+        this.lastName = Strings.nullToEmpty(lastName);
     }
 
     public String getSingleName() {
         return singleName;
     }
 
-    protected void setSingleName(String singleName) {
-        this.singleName = singleName;
+    public void setSingleName(String singleName) {
+        this.singleName = Strings.nullToEmpty(singleName);
     }
 
     public String getCellPhoneNumber() {
         return cellPhoneNumber;
     }
 
-    protected void setCellPhoneNumber(String cellPhoneNumber) {
-        this.cellPhoneNumber = cellPhoneNumber;
+    public void setCellPhoneNumber(String cellPhoneNumber) {
+        this.cellPhoneNumber = Strings.nullToEmpty(cellPhoneNumber);
     }
 
     public String getEmail() {
         return email;
     }
 
-    protected void setEmail(String email) {
-        this.email = email;
+    public void setEmail(String email) {
+        this.email = Strings.nullToEmpty(email);
     }
 
     public Collection<CommunicationMethod> getCommunicationMethods() {
-        return Collections.unmodifiableSet(communicationMethods);
+        return communicationMethods;
     }
 
-    protected void setCommunicationMethods(Set<CommunicationMethod> communicationMethods) {
-        this.communicationMethods = communicationMethods;
+    public void setCommunicationMethods(Set<CommunicationMethod> communicationMethods) {
+        this.communicationMethods = Objects.requireNonNull(communicationMethods);
     }
 
     public String getDisplayName() {
@@ -105,81 +113,10 @@ public class Contact extends AbstractFireDepartmentSpecificEntity {
         }
     }
 
-    public static class Builder extends AbstractFireDepartmentSpecificEntity.Builder<Contact, Builder> {
-
-        public Builder() {
-        }
-
-        public Builder(Contact original) {
-            super(original);
-            setCommunicationMethods(original.getCommunicationMethods());
-            setEmail(original.getEmail());
-            setCellPhoneNumber(original.getCellPhoneNumber());
-            setSingleName(original.getSingleName());
-            setLastName(original.getLastName());
-            setFirstName(original.getFirstName());
-        }
-
-        public Collection<CommunicationMethod> getCommunicationMethods() {
-            return getInstance().getCommunicationMethods();
-        }
-
-        public Builder setCommunicationMethods(Collection<CommunicationMethod> communicationMethods) {
-            if (communicationMethods == null) {
-                communicationMethods = Collections.emptySet();
-            }
-            getInstance().setCommunicationMethods(new HashSet<>(communicationMethods));
-            return myself();
-        }
-
-        public String getEmail() {
-            return getInstance().getEmail();
-        }
-
-        public Builder setEmail(String email) {
-            getInstance().setEmail(Strings.nullToEmpty(email));
-            return myself();
-        }
-
-        public String getCellPhoneNumber() {
-            return getInstance().getCellPhoneNumber();
-        }
-
-        public Builder setCellPhoneNumber(String cellPhoneNumber) {
-            getInstance().setCellPhoneNumber(Strings.nullToEmpty(cellPhoneNumber));
-            return myself();
-        }
-
-        public String getSingleName() {
-            return getInstance().getSingleName();
-        }
-
-        public Builder setSingleName(String singleName) {
-            getInstance().setSingleName(Strings.nullToEmpty(singleName));
-            return myself();
-        }
-
-        public String getLastName() {
-            return getInstance().getLastName();
-        }
-
-        public Builder setLastName(String lastName) {
-            getInstance().setLastName(Strings.nullToEmpty(lastName));
-            return myself();
-        }
-
-        public String getFirstName() {
-            return getInstance().getFirstName();
-        }
-
-        public Builder setFirstName(String firstName) {
-            getInstance().setFirstName(Strings.nullToEmpty(firstName));
-            return myself();
-        }
-
-        @Override
-        protected Contact newInstance() {
-            return new Contact();
-        }
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        final Contact clone = (Contact) super.clone();
+        clone.communicationMethods = new HashSet<>(communicationMethods);
+        return clone;
     }
 }
--- java/fenix-communication/src/main/java/net/pkhapps/fenix/communication/entity/ContactRepository.java
+++ java/fenix-communication/src/main/java/net/pkhapps/fenix/communication/entity/ContactRepository.java
@@ -1,9 +1,14 @@
 package net.pkhapps.fenix.communication.entity;
 
+import net.pkhapps.fenix.core.entity.FireDepartment;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * Repository of {@link net.pkhapps.fenix.communication.entity.Contact}s.
  */
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    List<Contact> findByFireDepartment(FireDepartment fireDepartment);
 }
