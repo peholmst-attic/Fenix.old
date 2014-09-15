46448f5ec51c8d3d1a8345593bfd3b471e483dce
Petter Holmström <petter@vaadin.com>
2014-09-16 00:13:49 +0300

    Configuration changes

--- java/fenix-app/src/main/resources/data.sql
+++ java/fenix-app/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO fire_departments (id, rev, name) VALUES (1, 1, 'Test Fire Department');
-INSERT INTO users (id, rev, first_name, last_name, encrypted_password, username, expired, locked, password_expired, enabled, fire_department_id) VALUES (1, 1, 'Joe', 'Cool', '$2a$04$1msDGF6OLTquNVDDA4FWPegdZrzLMzycrOkTdvmKn3OVFNlfw8q8u', 'joecool', false, false, false, true, 1);
+INSERT INTO fire_departments (id, rev, name, enabled) VALUES (1, 1, 'Test Fire Department', true);
+INSERT INTO users (id, rev, first_name, last_name, encrypted_password, username, password_exp_date, account_exp_date, locked, fire_department_id) VALUES (1, 1, 'Joe', 'Cool', '$2a$04$1msDGF6OLTquNVDDA4FWPegdZrzLMzycrOkTdvmKn3OVFNlfw8q8u', 'joecool', null, null, false, 1);
