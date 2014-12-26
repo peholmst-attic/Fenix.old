c7d3ec5940b82156a5ff544832cf9b9c5e2eff51
Petter Holmstroem <peholmst@abo.fi>
2014-12-26 22:05:02 +0200

    First draft of side bar

--- java/fenix-app/src/main/resources/public/js/app.js
+++ java/fenix-app/src/main/resources/public/js/app.js
@@ -1,4 +1,14 @@
 (function(){
     var fenixApp = angular.module('fenixApp', ['ngRoute']);
 
+    fenixApp.controller('SideBarSectionCtrl', function() {
+
+        this.isOpen = false;
+
+        this.toggle = function() {
+            this.isOpen = !this.isOpen;
+        };
+
+    });
+
 })();
