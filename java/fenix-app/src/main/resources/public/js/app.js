0077bc1b620f8bdf73539b69fa136b4fb9b15f38
Petter Holmstroem <peholmst@abo.fi>
2015-01-13 21:10:51 +0200

    WIP

--- java/fenix-app/src/main/resources/public/js/app.js
+++ java/fenix-app/src/main/resources/public/js/app.js
@@ -1,4 +1,28 @@
 (function(){
     var fxApp = angular.module('fxApp', ['ngRoute', 'fxSideBar']);
 
+    fxApp.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider
+            .when('/communication/new-message', {
+                templateUrl: 'partials/new-message.html'
+            })
+            .when('/communication/sent-messages', {
+                templateUrl: 'partials/sent-messages.html'
+            })
+            .when('/communication/contacts', {
+                templateUrl: 'partials/contact-list.html'
+            })
+            .when('/communication/groups', {
+                templateUrl: 'partials/group-list.html'
+            });
+            //.when('/phones/:phoneId', {
+            //    templateUrl: 'partials/phone-detail.html',
+            //    controller: 'PhoneDetailCtrl'
+            //});
+            //.otherwise({
+            //    redirectTo: '/phones'
+            //});
+        }
+    ]);
 })();
