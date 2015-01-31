86532d95118ee0720651764eac3e0f9a7b7019d9
Petter Holmström <petter.holmstrom@abo.fi>
2015-01-31 23:22:27 +0200

    Added fire department DTO and DTO mapper

---
+++ java/fenix-app/src/main/java/net/pkhapps/fenix/core/boundary/rest/dto/FireDepartmentDTO.java
@@ -0,0 +1,12 @@
+package net.pkhapps.fenix.core.boundary.rest.dto;
+
+import net.pkhapps.fenix.core.boundary.rest.AbstractEntityDTO;
+
+/**
+ * DTO representing a fire department.
+ */
+public class FireDepartmentDTO extends AbstractEntityDTO {
+
+    public String name;
+    public boolean enabled;
+}
---
+++ java/fenix-app/src/main/java/net/pkhapps/fenix/core/boundary/rest/dto/FireDepartmentDTOMapper.java
@@ -0,0 +1,22 @@
+package net.pkhapps.fenix.core.boundary.rest.dto;
+
+import net.pkhapps.fenix.core.boundary.rest.AbstractEntityDTOMapper;
+import net.pkhapps.fenix.core.entity.FireDepartment;
+import org.springframework.stereotype.Component;
+
+/**
+ * DTO mapper for {@link net.pkhapps.fenix.core.boundary.rest.dto.FireDepartmentDTO} and {@link net.pkhapps.fenix.core.entity.FireDepartment}.
+ */
+@Component
+public class FireDepartmentDTOMapper extends AbstractEntityDTOMapper<FireDepartmentDTO, FireDepartment> {
+
+    public FireDepartmentDTOMapper() {
+        super(FireDepartmentDTO.class, FireDepartment.class);
+    }
+
+    @Override
+    protected void populateDTO(FireDepartment source, FireDepartmentDTO destination) {
+        destination.name = source.getName();
+        destination.enabled = source.isEnabled();
+    }
+}
