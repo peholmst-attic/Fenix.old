051b2bf43e1b6a442027e3166d2b94c3c78dec4f
Petter Holmström <petter.holmstrom@abo.fi>
2015-02-09 23:08:20 +0200

    ContactCrud is fire dept specific

--- java/fenix-app/src/main/java/net/pkhapps/fenix/communication/control/ContactCrud.java
+++ java/fenix-app/src/main/java/net/pkhapps/fenix/communication/control/ContactCrud.java
@@ -1,10 +1,10 @@
 package net.pkhapps.fenix.communication.control;
 
 import net.pkhapps.fenix.communication.entity.Contact;
-import net.pkhapps.fenix.core.control.Crud;
+import net.pkhapps.fenix.core.control.FireDepartmentSpecificCrud;
 
 /**
  * {@link net.pkhapps.fenix.core.control.Crud CRUD} control for {@link net.pkhapps.fenix.communication.entity.Contact contacts}.
  */
-public interface ContactCrud extends Crud<Contact> {
+public interface ContactCrud extends FireDepartmentSpecificCrud<Contact> {
 }
--- java/fenix-app/src/main/java/net/pkhapps/fenix/communication/control/ContactCrudImpl.java
+++ java/fenix-app/src/main/java/net/pkhapps/fenix/communication/control/ContactCrudImpl.java
@@ -3,7 +3,7 @@ package net.pkhapps.fenix.communication.control;
 import net.pkhapps.fenix.communication.entity.Contact;
 import net.pkhapps.fenix.communication.entity.ContactRepository;
 import net.pkhapps.fenix.core.annotation.Control;
-import net.pkhapps.fenix.core.control.AbstractCrud;
+import net.pkhapps.fenix.core.control.AbstractFireDepartmentSpecificCrud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 
@@ -13,7 +13,7 @@ import javax.validation.Validator;
  * Default implementation of {@link net.pkhapps.fenix.communication.control.ContactCrud}.
  */
 @Control
-class ContactCrudImpl extends AbstractCrud<Contact, ContactRepository> implements ContactCrud {
+class ContactCrudImpl extends AbstractFireDepartmentSpecificCrud<Contact, ContactRepository> implements ContactCrud {
 
     @Autowired
     ContactCrudImpl(Validator validator, ContactRepository repository, ApplicationContext applicationContext) {
